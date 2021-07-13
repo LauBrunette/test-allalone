6e2419564729aa7488a5668e766dce9d25480ad0
LauBru <laurence.brunette@lapiscine.pro>
2021-07-13 14:59:56 +0200

    article insert form

--- src/Controller/Admin/AdminArticleController.php
+++ src/Controller/Admin/AdminArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Repository\ArticleRepository;
+use App\Form\ArticleType;
 use App\Repository\CategoryRepository;
 use App\Repository\TagRepository;
 use App\Entity\Article;
@@ -24,7 +25,7 @@ class AdminArticleController extends AbstractController
     // ArticleRepository : classe qui nous permet de faire ces requêtes
     // On doit donc instancier cette classe (grâce à l'autowire)
     // On place la classe ArticleRepository en argument du controller, suivi de la
-    //  variable dans laquelle on veut instancier la classe $articleRepository
+    // variable dans laquelle on veut instancier la classe $articleRepository
     public function articleList(ArticleRepository $articleRepository)
     {
 
@@ -41,49 +42,14 @@ class AdminArticleController extends AbstractController
     /**
      * @Route("/admin/articles/insert", name="admin_article_insert")
      */
-    public function insertArticle(
-        EntityManagerInterface $entityManager,
-        CategoryRepository $categoryRepository,
-        TagRepository $tagRepository
-    )
+    public function insertArticle()
     {
-        // J'utilise l'entité Article, pour créer un nouvel article en bdd
-        // une instance de l'entité Article = un enregistrement d'article en bdd
         $article = new Article();
+        $articleForm = $this->createForm(ArticleType::class, $article);
 
-        // j'utilise les setters de l'entité Article pour renseigner les valeurs
-        // des colonnes
-        $article->setTitle('Titre article depuis le controleur');
-        $article->setDescription('blablalbla');
-        $article->setIsPublished(true);
-        $article->setCreatedAt(new \DateTime('NOW'));
-
-        // je récupère la catégorie dont l'id est 1 en bdd
-        // doctrine me créé une instance de l'entité category avec les infos de la catégorie de la bdd
-        $category = $categoryRepository->find(1);
-        // j'associé l'instance de l'entité categorie récupérée, à l'instane de l'entité article que je suis
-        // en train de créer
-        $article->setCategory($category);
-
-        $tag = $tagRepository->findOneBy(['name' => 'info']);
-
-        if (is_null($tag)) {
-            $tag = new Tag();
-            $tag->setName("info");
-            $tag->setColor("yellow");
-        }
-
-        $entityManager->persist($tag);
-
-        $article->setTag($tag);
-
-        // je prends toutes les entités créées (ici une seule) et je les "pré-sauvegarde"
-        $entityManager->persist($article);
-
-        // je récupère toutes les entités pré-sauvegardées et je les insère en BDD
-        $entityManager->flush();
-
-        return $this->redirectToRoute("admin_article_list");
+        return $this->render('admin/admin_insert_article.html.twig', [
+            'articleForm' => $articleForm->createView()
+        ]);
     }
 
     /**
